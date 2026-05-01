d65f2639ec838f50066e6a0b22e95d5b47c982f3
Benny Chen <benny.chen@sjsu.edu>
2026-05-01 14:02:03 -0700

    eda changes and some fix

--- Website Test/pages/eda.py
+++ Website Test/pages/eda.py
@@ -21,14 +21,12 @@ hpai = pd.read_csv('https://raw.githubusercontent.com/bhagatjeetd/CS_163_project
 layout = html.Div([
     html.Div([
         html.H3("Dataset Preview"),
-        html.P("FAO Dataset"),
+        html.P("FAO"),
         dash_table.DataTable(data=fao.to_dict('records'), page_size=6),
-        #html.Img(src="/assets/fao_preview_original.png", style={"wdith": "100%"}),
         html.P(),
         html.P("Birds Affected"),
-        dash_table.DataTable(data=hpai.to_dict('records'), page_size=6)],
-        #html.Img(src="/assets/hpai_preview_original.png", style={"wdith": "50%"}),],
-        style=CARD_STYLE),
+        dash_table.DataTable(data=hpai.to_dict('records'), page_size=6)
+    ],style=CARD_STYLE),
     html.Div([
         html.H3("Price Statistics"),
         html.Div([
@@ -46,7 +44,36 @@ layout = html.Div([
             "justifyContent": "space-between",
             "gap": "4%"
         })
-    ], style=CARD_STYLE)
-    ],
+    ], style=CARD_STYLE),
+    html.Div([
+        html.H3("Distributions"),
+        html.Div([
+            html.Div([
+                html.Img(src="/assets/food_group_price_distribution.png", style={"wdith": "100%"}),
+                html.P("Boxplot of price distribution by food group"),
+            ], style={"width": "48%"}),
+            html.Div([
+                html.Img(src="/assets/overall_price_distribution.png", style={"wdith": "100%"}),
+                html.P("Histogram of overall price distribution"),
+            ], style={"width": "48%"})
+        ],
+        style={
+            "display": "flex",
+            "justifyContent": "space-between",
+            "gap": "4%"
+        })
+    ], style=CARD_STYLE),
+    html.Div([
+        html.H3("Egg Price vs Affected Bird Trend"),
+        html.Img(src="/assets/eggPrice_vs_affected.png", style={"wdith": "100%"}),
+        html.P("Overlaying trends showing egg price and affected bird count changes"),
+    ], style=CARD_STYLE),
+    html.Div([
+        html.H3("Insights"),
+        html.Li("Dairy and Protein products have the largest variability in their prices.", style={"padding": "10px"}),
+        html.Li("Majority of prices lie between 4 to 6, with right-skeweness and a notable max greater than 14.", style={"padding": "10px"}),
+        html.Li("When the amount of birds affected change, there is a small time lag until the egg prices change accordingly.", style={"padding": "10px"})
+    ], style=CARD_STYLE),
+],
     style={"padding": "20px", "backgroundColor": "#f4f6f9"}
 )
